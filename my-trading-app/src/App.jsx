eef98e473d18c841071f68bf587443700e6b3e56
NikitaDevda <nikitadevda178@gmail.com>
2026-01-07 17:53:14 +0530

    Update App.jsx

--- my-trading-app/src/App.jsx
+++ my-trading-app/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
         
   "overrides": {
     
-    "mainSeriesProperties.candleStyle.barSpacing": 4, // 
+    "mainSeriesProperties.candleStyle.barSpacing": 5, // 
     "paneProperties.topMargin": 10,
     "paneProperties.bottomMargin": 10,
   },
